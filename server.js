d31d5aa981ca649786cac37f84bb4fc22485ceb9
Celeste Rhoades <celesteisthebest82@gmail.com>
2024-04-09 11:37:40 -0600

    Motivational quotes

---
+++ server.js
@@ -0,0 +1,74 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const connectionString =
+  "mongodb+srv://celesteisthebest82:EUI8FdMifNUY2MVF@cluster0.xrukcbe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const MongoClient = require("mongodb").MongoClient;
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log("Connected to Database");
+    const db = client.db("Motivational-quotes");
+    const quotesCollection = db.collection("quotes");
+
+    app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.static("public"));
+    app.use(bodyParser.json());
+
+    app.get("/", (req, res) => {
+      quotesCollection
+        .find()
+        .toArray()
+        .then(results => {
+          res.render("index.ejs", { quotes: results });
+        })
+        .catch(error => console.error(error));
+    });
+
+    app.post("/quotes", (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then(result => {
+          res.redirect("/");
+        })
+        .catch(error => console.error(error));
+    });
+
+    app.put("/quotes", (req, res) => {
+      quotesCollection
+        .findOneAndUpdate(
+          { name: "Brene Brown" },
+          {
+            $set: {
+              name: req.body.name,
+              quote: req.body.quote,
+            },
+          },
+          {
+            upsert: true,
+          }
+        )
+        .then(result => {
+          res.json("Success");
+        })
+        .catch(error => console.error(error));
+    });
+    app.delete("/quotes", (req, res) => {
+      quotesCollection
+        .deleteOne({ name: req.body.name })
+        .then(result => {
+          if (result.deletedCount === 0) {
+            return res.json("No quote to delete");
+          }
+
+          res.json("Deleted Tony Robbins quote");
+        })
+        .catch(error => console.error(error));
+    });
+
+    app.listen(3000, function () {
+      console.log("Listening on 3000");
+    });
+  })
+  .catch(error => console.error(error));
